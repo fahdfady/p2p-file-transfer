25313ca0e366388a9a402a8357a19cc6ff208794
Fahd Fady <fahd.fady212@gmail.com>
2025-05-18 08:35:23 +0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>P2P File Transfer</h1>
+  </div>
+`
---
+++ src/rustcode/src/lib.rs
@@ -0,0 +1,2 @@
+// Chunk size in bytes (1MB)
+const CHUNK_SIZE: usize = 1_048_576;
